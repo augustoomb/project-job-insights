e4181ee3cee75e09c13c7cb42511acb0c515684c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-12-06 17:18:38 -0300

    feat: implementação da função filtered_by_industry no arquivo src/insights/industries.py para retornar uma lista de dicionários com todos os empregos onde a coluna industry corresponde ao parâmetro industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -23,21 +23,14 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    filtered_industries = [
+        job for job in jobs if (job["industry"] == industry)
+    ]
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return filtered_industries
+
+    # raise NotImplementedError
 
 
 # def main():
